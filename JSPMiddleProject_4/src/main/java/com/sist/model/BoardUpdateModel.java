8b85b791d2bf26ceac217bb21edf5bfd5dff13af
user <user@DESKTOP-9E0LFRE>
2024-07-29 08:35:34 +0900

    240725

---
+++ JSPMiddleProject_4/src/main/java/com/sist/model/BoardUpdateModel.java
@@ -0,0 +1,23 @@
+package com.sist.model;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.sist.dao.BoardDAO;
+import com.sist.dao.BoardVO;
+
+public class BoardUpdateModel implements Model {
+
+	@Override
+	public String execute(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		
+			   String no=request.getParameter("no");
+			   BoardDAO dao=BoardDAO.newInstance();
+			   BoardVO vo=dao.boardUpdateData(Integer.parseInt(no));
+			   request.setAttribute("vo", vo);
+			      
+		   
+		return "update.jsp";
+	}
+
+}
