a587ee8594c860bc43a8b7138a4b6cc9b5bd8f41
user <user@DESKTOP-9E0LFRE>
2024-07-29 08:38:12 +0900

    240719

---
+++ JSPMVCProject_1/src/main/java/com/sist/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.sist.controller;
+
+import javax.servlet.RequestDispatcher;
+
+public class Controller extends {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        RequestDispatcher rd=req
+	}
+
+}
---
+++ JSPMVCProject_1/src/main/java/com/sist/model/DeleteModel.java
@@ -0,0 +1,10 @@
+package com.sist.model;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class DeleteModel {
+   public String execute(HttpServletRequest request)
+   {
+	   String msg="게시판 목록";
+   }
+}
